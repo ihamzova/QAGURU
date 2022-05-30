0af602425d5f2ad0cbdfdc0fa15d9ee9d14b97db
Khamz <ihamzova@ya.ru>
2022-05-30 13:56:51 +0300

    second commit for HomeWork conflict

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -5,12 +5,12 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-    public class FirstTest {
-        @Test
-        void successfulSearchTest() {
-            open("https://www.google.com/");
-            $("[name=q]").setValue("selenide").pressEnter();
-            $("[id=search]").shouldHave(text("https://selenide.org"));
-            System.out.println("changes in domain needed");
- }
+public class FirstTest {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+        System.out.println("changes in domain needed because of different countries");
     }
+}
