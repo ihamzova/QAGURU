b504924ba05519b13e1e88f8013416515a4fb721
Irina Khamzova <irina.khamzova@t-systems.com>
2022-07-01 21:00:38 +0300

    added

--- src/test/java/qaGuru/Tests/TestBase.java
+++ src/test/java/qaGuru/Tests/TestBase.java
@@ -2,6 +2,7 @@ package qaGuru.Tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationFormPage;
 
 public class TestBase {
@@ -13,5 +14,10 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 }
