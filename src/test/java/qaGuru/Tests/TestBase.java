aca4d6adaf30190fdd622cedb970ce2e26f94949
ikhamzov <irina.khamzova@t-systems.com>
2022-07-13 16:07:27 +0300

    added

--- src/test/java/qaGuru/Tests/TestBase.java
+++ src/test/java/qaGuru/Tests/TestBase.java
@@ -18,22 +18,18 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
-
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.browser = System.getProperty("browser","chrome");
-        Configuration.browserVersion = System.getProperty("version","100");
-        Configuration.browserSize = System.getProperty("browserSize", "3840x2160");
-        // Configuration.holdBrowserOpen = true;
-        Configuration.remote = String.format("https://%s:%s@%s", config.login(), config.password(), config.urlCloudSelenoid());
     }
     @AfterEach
-    void addAttachments() {
+    void afterEach() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
